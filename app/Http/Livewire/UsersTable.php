97c7b35e22aec26b7f66a16da3e3b31a097e43dc
Nathan <baumnathan@gmail.com>
2021-06-14 20:26:40 -0400

    init

---
+++ app/Http/Livewire/UsersTable.php
@@ -0,0 +1,61 @@
+<?php
+
+namespace App\Http\Livewire;
+
+use App\Models\User;
+use Rappasoft\LaravelLivewireTables\DataTableComponent;
+use Rappasoft\LaravelLivewireTables\Views\Column;
+use Rappasoft\LaravelLivewireTables\Views\Filter;
+
+class UsersTable extends DataTableComponent
+{
+    public $users;
+
+    public function mount()
+    {
+        $this->users = User::query()
+                           ->get(['id','name'])
+                           ->pluck('name','id')
+                           ->toArray();
+    }
+
+    public function columns(): array
+    {
+        return [
+            Column::make('Name')
+                  ->sortable()
+                  ->searchable(),
+            Column::make('E-mail', 'email')
+                  ->sortable()
+                  ->searchable(),
+            Column::make('Verified', 'email_verified_at')
+                  ->sortable(),
+        ];
+    }
+
+    public function filters(): array
+    {
+
+        return [
+            'active' => Filter::make('User Name')
+                              ->select([
+                                  '' => 'Any',
+                                  ...$this->users
+                              ]),
+            'verified' => Filter::make('E-mail Verified')
+                                ->select([
+                                    '' => 'Any',
+                                    'yes' => 'Yes',
+                                    'no' => 'No',
+                                ]),
+        ];
+    }
+
+    /**
+     * @return \Illuminate\Database\Eloquent\Builder|\Illuminate\Database\Eloquent\Relations\Relation
+     */
+    public function query()
+    {
+        return User::query();
+    }
+}
